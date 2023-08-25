a48499830176dc50ecbb86214a8f3bf3c7a6e795
Tooboi <japica123@gmail.com>
2023-08-25 18:33:44 -0400

    Added ArtStation button

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,6 @@
 import Blender from '../images/blender.png';
 import Substance from '../images/substance.png';
+import { ReactComponent as Artststion } from '../images/artstation.svg';
 
 export default function Project({ projects, currentTab }) {
   function Logo({ logoOption }) {
@@ -32,7 +33,7 @@ export default function Project({ projects, currentTab }) {
         <div className="p-2" key={project.id}>
           <div className="mx-auto max-w-[512px] min-w-[150px] aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-stone-800">
             <section className="absolute z-10 opacity-0 hover:opacity-100 transition">
-              <div className="image-gradient h-full w-full" />
+              <div className="image-gradient h-full w-full flex flex-row" />
               <div className="z-20 absolute bottom-0 left-0 p-4">
                 <p className="text-2xl font-medium text-stone-200 text-left select-none">{project.title}</p>
                 <p className="text-md font-medium text-stone-200 select-none">{project.year}</p>
@@ -40,6 +41,11 @@ export default function Project({ projects, currentTab }) {
                 <div className="">
                   <Logo logoOption={project.logo} />
                 </div>
+              </div>
+              <div className="z-20 absolute bottom-0 right-0 p-4">
+                <a href={project.link} target="_blank" rel="noreferrer">
+                  <Artststion className="w-6 mx-2 fill-stone-700 hover:fill-stone-600 hover:scale-105 transition-all active:scale-100" />
+                </a>
               </div>
             </section>
             <img src={project.image} alt={project.title} className="object-cover" />
